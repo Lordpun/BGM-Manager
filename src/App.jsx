ad448ddc1b995986e8f566930223b6230842c47b
Lordpun <thedirtcart@gmail.com>
2026-06-23 20:38:15 -0400

    Made a simple layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-  return (<></>);
+  return (<>
+    <header className="text-center border-2 border-bottom pt-3">
+      <h1>BGM Manager</h1>
+      <p className="fs-6 text-muted">Yes, that says Background Manager Manager</p>
+    </header>
+
+    <main className="pt-3 d-flex flex-column" style={{ minHeight: 'calc(100vh - 8rem)' }}>
+      <section className="border border-2 w-75 flex-grow-1 text-center mx-auto overflow-scroll">
+        <p>Your Groups</p>
+      </section>
+      <a className="btn btn-info fw-bold text-white mt-3 mx-auto mb-3">Add Background Group</a>
+    </main>
+  </>);
 }
 
 export default App;
