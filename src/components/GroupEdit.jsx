321e47db11d46f71a0fb17ca4633e9245811ac38
Lordpun <thedirtcart@gmail.com>
2026-06-26 22:01:27 -0400

    Added the option for light or dark text color. Added a warning to closing

--- src/components/GroupEdit.jsx
+++ src/components/GroupEdit.jsx
@@ -1,4 +1,11 @@
 function GroupEdit({ groupData, closePopup }) {
+  const closeWithoutSave = async () => {
+    let response = confirm("Close without saving?");
+    if (response) {
+      closePopup();
+    }
+  }
+
   const saveData = async () => {
     closePopup();
   }
@@ -10,24 +17,38 @@ function GroupEdit({ groupData, closePopup }) {
     <h1 className="border-bottom border-2">Edit Group</h1>
 
     <section className="mt-4 d-flex flex-column" style={{ minHeight: 'calc(100% - 4rem)' }}>
-      <form className="flex-grow-1">
+      <form className="flex-grow-1 overflow-scroll">
         <div>
           <label>Group Name</label>
           <br/>
           <input name="name" type="text" placeholder="Name" required/>
         </div>
-        <div className="mt-2">
-          <input name="changeColor" type="checkbox"/>
+
+        <div className="mt-3">
+          <input className="me-2" name="changeColor" type="checkbox"/>
           <label>Change Terminal Color</label>
         </div>
-        <div className="mt-2">
-          <input name="changeKvantum" type="checkBox"/>
+
+        <div className="mt-3">
+          <input className="me-2" name="changeKvantum" type="checkBox"/>
           <label>Change Kvantum Color - <span className="text-danger fw-bold">Hardcoded for Glassy theme</span></label>
+        </div>
+
+        <div className="mt-3">
+          <label>Text Color</label>
+          <div>
+            <input className="me-2" name="textColor" type="radio" value="light"/>
+            <label>Light</label>
+          </div>
+          <div>
+            <input className="me-2" name="textColor" type="radio" value="dark"/>
+            <label>Dark</label>
+          </div>
         </div>
       </form>
 
       <section className="mt-auto text-end">
-        <a className="btn btn-warning me-2" onClick={closePopup}>Close</a>
+        <a className="btn btn-warning me-2" onClick={closeWithoutSave}>Close</a>
         <a className="btn btn-info">Save</a>
       </section>
     </section>
